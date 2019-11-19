7bbef9477427e5175d04933baf3f6dafa8bb42c1
nicholasborrelli <nicholasaborrelli@gmail.com>
2019-11-19 14:16:28 -0500

    commit 6

--- src/WeightedAvgDropSmallest.java
+++ src/WeightedAvgDropSmallest.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class WeightedAvgDropSmallest {
 
+    /**
+     *
+     * @return nums which is the ArrayList of numbers scanned by the user
+     */
     public static ArrayList numbers() {
         ArrayList<Double> nums = new ArrayList<Double>();
         Scanner scan = new Scanner(System.in);
@@ -12,6 +16,10 @@ public class WeightedAvgDropSmallest {
         return nums;
     }
 
+    /**
+     *
+     * @return drop is an int which is scanned from the user on how many grades they want dropped
+     */
     public static int drop() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter how many numbers do you want to drop: ");
@@ -19,6 +27,10 @@ public class WeightedAvgDropSmallest {
         return drop;
     }
 
+    /**
+     *
+     * @return weight of the grades which is scanned from the user
+     */
     public static double weight() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the weight: ");
@@ -26,6 +38,13 @@ public class WeightedAvgDropSmallest {
         return weight;
     }
 
+    /**
+     *
+     * @param nums which is the ArrayList of numbers scanned by the user
+     * @param drop is an int which is scanned from the user on how many grades they want dropped
+     * @param weight of the grades which is scanned from the user
+     * @return average of the grades that does not include the dropped grades and is multiplied by the weight
+     */
     public static double averageCalc(ArrayList<Double> nums, int drop, double weight) {
         double minVal = nums.get(0);
         for (int i = 0; i < drop; i++) {
@@ -44,10 +63,18 @@ public class WeightedAvgDropSmallest {
         return average;
     }
 
+    /**
+     *
+     * @param average of the grades that does not include the dropped grades and is multiplied by the weight
+     */
     public static void printResults(double average){
         System.out.printf("The average is: %.2f", average);
     }
 
+    /**
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         System.out.println(" ");
         printResults(averageCalc(numbers(), drop(), weight()));
